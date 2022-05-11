8f6b1c64f364dc295dde949734f0ffe8170e3dc6
joyce.olympio <joyoly@on.sinch.com>
2022-05-11 14:46:11 -0300

    primeiro commit teste

---
+++ src/projectGit/calculoMedia.java
@@ -0,0 +1,35 @@
+package projectGit;
+
+import java.util.Scanner;
+
+public class calculoMedia {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		double nota1, nota2, nota3, media;
+		
+		System.out.println("Digite a primeira nota: ");
+		nota1 = entrada.nextDouble();
+		
+		System.out.println("Digite a segunda nota: ");
+		nota2 = entrada.nextDouble();
+		
+		System.out.println("Digite a terceira nota: ");
+		nota3 = entrada.nextDouble();
+		
+		media = (nota1 + nota2 + nota3)/3;
+		
+		System.out.printf("Sua média é : %.1f \n", media);
+		
+		if (media >= 7) {
+			System.out.println("Você foi aprovado!");
+		} else if (media >= 5) {
+			System.out.println("Você ficou de recuperação!");
+		} else {
+			System.out.println("Você foi reprovado!");
+		}
+		
+		entrada.close();
+	}
+}
